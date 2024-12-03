68dcb7f5c2b82125a3678c49d4cdb6128407f4e6
Bush Nguyen <bushnguyen@bushs-mbp.attlocal.net>
2024-12-03 01:35:47 -0800

    protect route middleware

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getAdmin } from "../controller/admin.controller.js";
+import { createSong } from "../controller/admin.controller.js";
+import { protectRoute, requireAdmiin } from "../middleware/auth.middleware.js";
 
 const router = Router()
 
-router.get("/", getAdmin)
+router.get("/", protectRoute, requireAdmiin, createSong)
 
 export default router
